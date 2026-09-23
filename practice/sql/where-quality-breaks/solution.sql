12cf05ab230eff3f4a72709166be20e89f91bb64
Ashok Shah <sa.ashokshah@gmail.com>
2026-09-23 13:26:35 -0500

    Solve where-quality-breaks

--- practice/sql/where-quality-breaks/solution.sql
+++ practice/sql/where-quality-breaks/solution.sql
@@ -1,7 +1,11 @@
 SELECT
   tbl_name,
-  AVG(fail_pct) AS avg_fail_pct
-FROM dq_checks
-GROUP BY tbl_name
-HAVING COUNT(DISTINCT rule) > 1
-ORDER BY avg_fail_pct DESC, tbl_name
+  COUNT(passed) AS failed_checks,
+  COUNT(distinct rule) as distinct_rules,
+  AVG(fail_pct) as avg_fail_pct
+FROM 
+  dq_checks
+where passed = 0
+GROUP BY 1
+HAVING avg_fail_pct > 30
+order by 4 desc
