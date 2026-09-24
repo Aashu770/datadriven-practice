308f514dc382b85420a9519db592dbb66fa246ea
Ashok Shah <sa.ashokshah@gmail.com>
2026-09-24 16:43:15 -0500

    Solve daily-error-count-change

--- practice/sql/daily-error-count-change/solution.sql
+++ practice/sql/daily-error-count-change/solution.sql
@@ -1,21 +1,15 @@
-with base as (
-  select 
-      date(first_at) as error_date, 
-      count(distinct err_type) as error_count
-  from err_tracks
-  group by 1
- ),
- cal as (
-    select 
-        error_date,
-        error_count, 
-        LAG(error_count) OVER(ORDER BY error_date) as prev_count
-    from base 
-  )
-  select 
-      error_date, 
-      error_count, 
-      prev_count,
-      error_count - prev_count as day_over_day_change
-  from cal 
-  order by 1
+WITH daily_errors AS (
+    SELECT
+        DATE(first_at) as error_date,
+        COUNT(severity) as error_count
+    FROM err_tracks
+    --WHERE severity = 'error'
+    GROUP BY 1
+)
+SELECT
+    error_date,
+    error_count,
+    LAG(error_count) OVER (ORDER BY error_date) as prev_count,
+    error_count - LAG(error_count) OVER (ORDER BY error_date) as day_over_day_change
+FROM daily_errors
+ORDER BY error_date
